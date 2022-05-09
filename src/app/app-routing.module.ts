4815cab2ffcaf2eb9b43010d261fc3546a7af668
mukulkumar-crownstack <manchikantisaikiran777@gmail.com>
2022-05-09 08:09:56 +0530

    added ssr

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SkillsComponent } from './components/skills/skills.component';
+import { WorkExperienceComponent } from './components/work-experience/work-experience.component';
 import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
+  { path: 'carousel', loadChildren: () => import('./carousel/carousel.module').then(m => m.CarouselModule) },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'work-experience', component:WorkExperienceComponent },
   {
     path: '',
     component: HomeComponent,
@@ -10,7 +15,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation: 'enabled'
+})],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,7 @@ import { FooterComponent } from './components/footer/footer.component';
     FooterComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule
   ],
   providers: [],
